f1079cf88450df473e16b181a0fa08dd99018130
Enrico Stefanel <me@enst.it>
2022-09-11 11:10:40 +0200

    Fix empty notification for non-collecting day

--- send_wastecollection_notify.sh
+++ send_wastecollection_notify.sh
@@ -6,8 +6,11 @@ source <(grep = /home/enstit/scripts/udine_wastecollection_reminder/config.ini)
 # Get the current day of week in locale’s full weekday name (e.g. "Monday")
 DAY_OF_WEEK=$(date --date="tomorrow" +%A)
 
-curl -s \
-  --data "text=♻️ Remember to bring out the *${!DAY_OF_WEEK,,} bin* for the collection!" \
-  --data "chat_id=${GROUP_ID}" \
-  --data-urlencode "parse_mode=markdown" \
-  'https://api.telegram.org/bot'${BOT_TOKEN}'/sendMessage' >> /tmp/udine_wastecollection_reminder.log
+if [ ! -z "${!DAY_OF_WEEK,,}" ]
+then
+  curl -s \
+    --data "text=♻️ Remember to bring out the *${!DAY_OF_WEEK,,} bin* for the collection!" \
+    --data "chat_id=${GROUP_ID}" \
+    --data-urlencode "parse_mode=markdown" \
+    'https://api.telegram.org/bot'${BOT_TOKEN}'/sendMessage' >> /tmp/udine_wastecollection_reminder.log
+fi
